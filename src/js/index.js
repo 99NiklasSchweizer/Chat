317df0b4f753991fdb70de7e688b10bf3b344f8d
Niklas Schweizer <dumblexx@gmail.com>
2018-10-17 10:48:48 +0200

    Lagt till chat dialogen

--- src/js/index.js
+++ src/js/index.js
@@ -5,9 +5,26 @@ import password from './config.js';
 
 const connection = new WebSocket('ws://104.248.143.87:1337');
 
-const message = document.getElementById('textInput');
-const send = document.getElementById('textSendBtn');
+const message = document.getElementById('msgInput');
+const send = document.getElementById('msgSendBtn');
 
+connection.onmessage = message => {
+  const msg = JSON.parse(message.data);
+  console.log(msg.data.text);
+
+  const template = document.querySelector('#chatWindow template');
+  console.log(template);
+  const tempateDiv = template.content.querySelector('#msgWindow');
+
+  const messageChat = document.importNode(tempateDiv, true);
+  const chatWindow = document.getElementById('chatWindow');
+  const msgPos = document.getElementById('msgPos');
+  console.log(chatWindow);
+  console.log(msgPos);
+  messageChat.textContent = msg.data.text;
+
+  msgPos.appendChild(messageChat);
+};
 send.addEventListener('click', () => {
   const obj = {
     type: 'message',
@@ -16,4 +33,6 @@ send.addEventListener('click', () => {
 
     key: password
   };
+  const jsonObj = JSON.stringify(obj);
+  connection.send(jsonObj);
 });
