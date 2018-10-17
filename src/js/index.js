cb04e6178265b1889f1dcea7cd9ac6cbf4a4cc94
Niklas Schweizer <dumblexx@gmail.com>
2018-10-17 08:36:10 +0200

    Lagt till event listener för send knapp

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
+import password from './config.js';
+
+const connection = new WebSocket('ws://104.248.143.87:1337');
+
+const message = document.getElementById('textInput');
+const send = document.getElementById('textSendBtn');
+
+send.addEventListener('click', () => {
+  const obj = {
+    type: 'message',
+
+    data: message.value,
+
+    key: password
+  };
+});
